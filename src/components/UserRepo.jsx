6a90c920e0f0a56f0e314e03eabfe0f5707f2c1c
Inem Patrick <emmanuelpatrick25@gmail.com>
2019-04-27 02:03:41 +0100

    Initial commit

---
+++ src/components/UserRepo.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const UserRepo = () => {
+  const [name, setName] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    getRepos();
+  };
+  const handleNameChange = e => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const getRepos = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.github.com/users/${name}/repos`
+      );
+      setRepos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input value={name} onChange={handleNameChange} />
+        <submit>Search</submit>
+      </form>
+      <div>
+        {repos ? repos.map(repo => <div>{repo.name}</div>) : "No repos"}
+      </div>
+    </section>
+  );
+};
+
+export default UserRepo;
